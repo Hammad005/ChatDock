1834518c1be68246673231abb5c7d8c76857fa10
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-08-13 14:00:37 +0500

    ✨ feat(server): implement user authentication and database connection
    - integrate mongoose to connect to mongodb database
    - create user model with schema and password hashing
    - implement connectDB function to establish database connection
    - connect to database before starting the server

---
+++ server/src/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+    },
+    profilePic: {
+        imageId: {
+            type: String,
+            default: null
+        },
+        imageUrl: {
+            type: String,
+            default: null
+        },
+        isUpdated: {
+            type: Boolean,
+            default: false
+        },
+    }
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import 'dotenv/config';
+import connectDb from "./config/connectDB.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -8,4 +9,5 @@ app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    connectDb();
 });
